9131dc667d2fd8d73e789defe5cc1f4ecc14d675
JiuZero <1703417187@qq.com>
2026-07-04 15:43:16 +0800

    Site updated: 2026-07-04 15:43:14

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const VERSION = "1783142230445";const preCache = ["/images/taichi.png","/images/banner.webp","/css/loader.css","/css/style.css","/js/script.js"];const cacheDomain = [
+const VERSION = "1783150975826";const preCache = ["/images/taichi.png","/images/banner.webp","/css/loader.css","/css/style.css","/js/script.js"];const cacheDomain = [
   "fonts.googleapis.com",
   "npm.webcache.cn",
   "unpkg.com",
